ff0599e558118717f6ea1dad9d52f12a8492aa99
DemonSinusa <tonich89@gmail.com>
2018-10-12 17:28:36 +0300

    Подправлена обработка конфигов (sscanf и пробелы)

--- TerminimalBootEdit/Config.cpp
+++ TerminimalBootEdit/Config.cpp
@@ -49,12 +49,12 @@ OPT options[NUM_PROPS];
 void Config::SetType(OPT *item, int i)
 {
     if (i == PROP_KERNEL_ADDR || i == PROP_KERNELTAG_ADDR ||
-            i == PROP_ROOTFS_ADDR || i == PROP_SECONDFS_ADDR)
-        item->datatype = TYPE_HEX;
+	    i == PROP_ROOTFS_ADDR || i == PROP_SECONDFS_ADDR)
+	item->datatype = TYPE_HEX;
     else if (i == MIX_OS_VERSION || i == PROP_NAME || i == PROP_CMDLINE || i == PROP_CMDLINE_EX)
-        item->datatype = TYPE_TXT;
+	item->datatype = TYPE_TXT;
     else
-        item->datatype = TYPE_INT;
+	item->datatype = TYPE_INT;
 }
 
 int Config::SetParm(UfNtype *str, const char *list[])
@@ -67,19 +67,19 @@ int Config::SetParm(UfNtype *str, const char *list[])
     strncpy(lexem, str, len);
     lexem[len] = '\0';
     for (i = 0; i < NUM_PROPS; i++) {
-        if (strstr(lexem, list[i]))
-            break;
+	if (strstr(lexem, list[i]))
+	    break;
     }
     delete[] lexem;
     if (i != NUM_PROPS) {
-        if (len > 0) {
-            options[i].len = &str[strlen(str)]-(var + 1);
-            strncpy(options[i].txtname, var + 1, MAX_PARAMETR_LEN);
-            SetType(&options[i], i);
-        }
-        return i;
+	if (len > 0) {
+	    options[i].len = &str[strlen(str)]-(var + 1);
+	    strncpy(options[i].txtname, var + 1, MAX_PARAMETR_LEN);
+	    SetType(&options[i], i);
+	}
+	return i;
     } else
-        return -1;
+	return -1;
 }
 
 int Config::PackOSVersion(OSV *version)
@@ -97,24 +97,24 @@ void Config::UnpackOSVersion(OSV *version, int packed)
 {
     memset(version, 0x00, sizeof (OSV));
     if (packed != 0) {
-        int os_version, os_patch_level;
-        os_version = packed >> 11;
-        os_patch_level = packed & 0x7ff;
+	int os_version, os_patch_level;
+	os_version = packed >> 11;
+	os_patch_level = packed & 0x7ff;
 
-        version->major = (os_version >> 14)&0x7f;
-        version->minor = (os_version >> 7)&0x7f;
-        version->patch = os_version & 0x7f;
+	version->major = (os_version >> 14)&0x7f;
+	version->minor = (os_version >> 7)&0x7f;
+	version->patch = os_version & 0x7f;
 
-        version->year = (os_patch_level >> 4) + 2000;
-        version->month = os_patch_level & 0xf;
+	version->year = (os_patch_level >> 4) + 2000;
+	version->month = os_patch_level & 0xf;
 
-        if ((version->major < 128) && (version->minor < 128) && (version->patch < 128) &&
-                (version->year >= 2000) && (version->year < 2128) && (version->month > 0) && (version->month <= 12)) {
-            printf("BOARD_OS_VERSION %d.%d.%d\n", version->major, version->minor, version->patch);
-            printf("BOARD_OS_PATCH_LEVEL %d-%02d\n", version->year, version->month);
-        } else {
-            packed = 0;
-        }
+	if ((version->major < 128) && (version->minor < 128) && (version->patch < 128) &&
+		(version->year >= 2000) && (version->year < 2128) && (version->month > 0) && (version->month <= 12)) {
+	    printf("BOARD_OS_VERSION %d.%d.%d\n", version->major, version->minor, version->patch);
+	    printf("BOARD_OS_PATCH_LEVEL %d-%02d\n", version->year, version->month);
+	} else {
+	    packed = 0;
+	}
     }
 }
 
@@ -127,41 +127,41 @@ Config::Config()
 Config::~Config()
 {
     if (txtblk)
-        free(txtblk);
+	free(txtblk);
     if (block)
-        free(block);
+	free(block);
 }
 
 int Config::EatBinConfig(UfNtype *file)
 {
     if ((fc = fopen(file, "rb")) != NULL) {
-        fseek(fc, 0x00, SEEK_END);
-        blklen = ftell(fc);
-        fseek(fc, 0x00, SEEK_SET);
-        if (block)
-            free(block);
-        block = (char *) malloc(blklen);
-        fread(block, blklen, 1, fc);
-        fclose(fc);
-        return EatBinConfig((void *) block, blklen);
+	fseek(fc, 0x00, SEEK_END);
+	blklen = ftell(fc);
+	fseek(fc, 0x00, SEEK_SET);
+	if (block)
+	    free(block);
+	block = (char *) malloc(blklen);
+	fread(block, blklen, 1, fc);
+	fclose(fc);
+	return EatBinConfig((void *) block, blklen);
     } else
-        return 0;
+	return 0;
 }
 
 int Config::EatTxtConfig(UfNtype *file)
 {
     if ((fc = fopen(file, "rb")) != NULL) {
-        fseek(fc, 0x00, SEEK_END);
-        tblklen = ftell(fc);
-        fseek(fc, 0x00, SEEK_SET);
-        if (txtblk)
-            free(txtblk);
-        txtblk = (char *) malloc(tblklen);
-        fread(txtblk, tblklen, 1, fc);
-        fclose(fc);
-        return EatTxtConfig((void *) txtblk);
+	fseek(fc, 0x00, SEEK_END);
+	tblklen = ftell(fc);
+	fseek(fc, 0x00, SEEK_SET);
+	if (txtblk)
+	    free(txtblk);
+	txtblk = (char *) malloc(tblklen);
+	fread(txtblk, tblklen, 1, fc);
+	fclose(fc);
+	return EatTxtConfig((void *) txtblk);
     } else
-        return 0;
+	return 0;
 }
 
 int Config::EatBinConfig(void *data, unsigned int len)
@@ -169,72 +169,72 @@ int Config::EatBinConfig(void *data, unsigned int len)
     int parlen = 0;
     boot_img_hdr *curboot = NULL;
     if (len >= sizeof (boot_img_hdr))
-        curboot = (boot_img_hdr *) data;
+	curboot = (boot_img_hdr *) data;
     else
-        return 0;
+	return 0;
     //Ручками перетаскиваем каждый параметр
     memset(options, 0x00, sizeof (OPT) * NUM_PROPS);
 
     if ((parlen = strlen((char *) curboot->cmdline)) != 0) {
-        strncpy(options[PROP_CMDLINE].txtname, (char *) curboot->cmdline, MAX_PARAMETR_LEN);
-        options[PROP_CMDLINE].len = parlen;
-        SetType(&options[PROP_CMDLINE], PROP_CMDLINE);
+	strncpy(options[PROP_CMDLINE].txtname, (char *) curboot->cmdline, MAX_PARAMETR_LEN);
+	options[PROP_CMDLINE].len = parlen;
+	SetType(&options[PROP_CMDLINE], PROP_CMDLINE);
     }
     if ((parlen = strlen((char *) curboot->extra_cmdline)) != 0) {
-        strncpy(options[PROP_CMDLINE_EX].txtname, (char *) curboot->extra_cmdline, MAX_PARAMETR_LEN);
-        options[PROP_CMDLINE_EX].len = parlen;
-        SetType(&options[PROP_CMDLINE_EX], PROP_CMDLINE_EX);
+	strncpy(options[PROP_CMDLINE_EX].txtname, (char *) curboot->extra_cmdline, MAX_PARAMETR_LEN);
+	options[PROP_CMDLINE_EX].len = parlen;
+	SetType(&options[PROP_CMDLINE_EX], PROP_CMDLINE_EX);
     }
     if (curboot->kernel_addr > 0) {
-        sprintf(options[PROP_KERNEL_ADDR].txtname, "0x%08x", curboot->kernel_addr);
-        options[PROP_KERNEL_ADDR].len = sizeof (uint32_t);
-        SetType(&options[PROP_KERNEL_ADDR], PROP_KERNEL_ADDR);
+	sprintf(options[PROP_KERNEL_ADDR].txtname, "0x%08x", curboot->kernel_addr);
+	options[PROP_KERNEL_ADDR].len = sizeof (uint32_t);
+	SetType(&options[PROP_KERNEL_ADDR], PROP_KERNEL_ADDR);
     }
 
     if (curboot->tags_addr > 0) {
-        sprintf(options[PROP_KERNELTAG_ADDR].txtname, "0x%08x", curboot->tags_addr);
-        options[PROP_KERNELTAG_ADDR].len = sizeof (uint32_t);
-        SetType(&options[PROP_KERNELTAG_ADDR], PROP_KERNELTAG_ADDR);
+	sprintf(options[PROP_KERNELTAG_ADDR].txtname, "0x%08x", curboot->tags_addr);
+	options[PROP_KERNELTAG_ADDR].len = sizeof (uint32_t);
+	SetType(&options[PROP_KERNELTAG_ADDR], PROP_KERNELTAG_ADDR);
     }
 
     if (curboot->ramdisk_addr > 0) {
-        sprintf(options[PROP_ROOTFS_ADDR].txtname, "0x%08x", curboot->ramdisk_addr);
-        options[PROP_ROOTFS_ADDR].len = sizeof (uint32_t);
-        SetType(&options[PROP_ROOTFS_ADDR], PROP_ROOTFS_ADDR);
+	sprintf(options[PROP_ROOTFS_ADDR].txtname, "0x%08x", curboot->ramdisk_addr);
+	options[PROP_ROOTFS_ADDR].len = sizeof (uint32_t);
+	SetType(&options[PROP_ROOTFS_ADDR], PROP_ROOTFS_ADDR);
     }
     if (curboot->second_addr > 0) {
-        sprintf(options[PROP_SECONDFS_ADDR].txtname, "0x%08x", curboot->second_addr);
-        options[PROP_SECONDFS_ADDR].len = sizeof (uint32_t);
-        SetType(&options[PROP_SECONDFS_ADDR], PROP_SECONDFS_ADDR);
+	sprintf(options[PROP_SECONDFS_ADDR].txtname, "0x%08x", curboot->second_addr);
+	options[PROP_SECONDFS_ADDR].len = sizeof (uint32_t);
+	SetType(&options[PROP_SECONDFS_ADDR], PROP_SECONDFS_ADDR);
     }
     if (curboot->page_size > 0) {
-        sprintf(options[PROP_PAGE_SIZE].txtname, "%d", curboot->page_size);
-        options[PROP_PAGE_SIZE].len = sizeof (uint32_t);
-        SetType(&options[PROP_PAGE_SIZE], PROP_PAGE_SIZE);
+	sprintf(options[PROP_PAGE_SIZE].txtname, "%d", curboot->page_size);
+	options[PROP_PAGE_SIZE].len = sizeof (uint32_t);
+	SetType(&options[PROP_PAGE_SIZE], PROP_PAGE_SIZE);
     } else
-        return 0;
+	return 0;
 
     if (curboot->os_version > 0) {
-        UnpackOSVersion(&ver, curboot->os_version);
-        sprintf(options[MIX_OS_VERSION].txtname, "%d.%d.%d %02d/%d", ver.major, ver.minor, ver.patch, ver.month, ver.year);
-        options[MIX_OS_VERSION].len = strlen(options[MIX_OS_VERSION].txtname);
-        SetType(&options[MIX_OS_VERSION], MIX_OS_VERSION);
+	UnpackOSVersion(&ver, curboot->os_version);
+	sprintf(options[MIX_OS_VERSION].txtname, "%d.%d.%d %02d/%d", ver.major, ver.minor, ver.patch, ver.month, ver.year);
+	options[MIX_OS_VERSION].len = strlen(options[MIX_OS_VERSION].txtname);
+	SetType(&options[MIX_OS_VERSION], MIX_OS_VERSION);
     }
     if ((parlen = strlen((char *) curboot->name)) != 0) {
-        strncpy(options[PROP_NAME].txtname, (char *) curboot->name, MAX_PARAMETR_LEN);
-        options[PROP_NAME].len = parlen;
-        SetType(&options[PROP_NAME], PROP_NAME);
+	strncpy(options[PROP_NAME].txtname, (char *) curboot->name, MAX_PARAMETR_LEN);
+	options[PROP_NAME].len = parlen;
+	SetType(&options[PROP_NAME], PROP_NAME);
     }
 
     int i, counto = 0;
 
     for (i = 0; i < 8; i++) {
-        sprintf(&options[ID0].txtname[i * 8], "%08x", curboot->id[i]);
-        //	    sprintf(options[ID0 + i].txtname, "%08x", curboot->id[i]);
-        //	    SetType(&options[ID0], ID0);
-        //	    options[ID0].len = sizeof (uint32_t);
-        if (curboot->id[i] > 0)
-            counto++;
+	sprintf(&options[ID0].txtname[i * 8], "%08x", curboot->id[i]);
+	//	    sprintf(options[ID0 + i].txtname, "%08x", curboot->id[i]);
+	//	    SetType(&options[ID0], ID0);
+	//	    options[ID0].len = sizeof (uint32_t);
+	if (curboot->id[i] > 0)
+	    counto++;
     }
     SetType(&options[ID0], ID0);
     options[ID0].len = counto * sizeof (uint32_t);
@@ -247,17 +247,17 @@ int Config::EatTxtConfig(void *data)
     int len = 0;
     UfNtype *str = (UfNtype *) data, *src = (UfNtype *) data;
     while (len < tblklen) {
-        if (src[len] == '\r' || src[len] == '\n') {
-            src[len] = '\0';
-            if (str) {
-                SetParm(str, names);
-                if (src[len + 1] == '\r' || src[len + 1] == '\n')
-                    str = NULL;
+	if (src[len] == '\r' || src[len] == '\n') {
+	    src[len] = '\0';
+	    if (str) {
+		SetParm(str, names);
+		if (src[len + 1] == '\r' || src[len + 1] == '\n')
+		    str = NULL;
 
-            } else
-                str = &src[len + 1];
-        }
-        len++;
+	    } else
+		str = &src[len + 1];
+	}
+	len++;
     }
     return 1;
 }
@@ -267,40 +267,40 @@ boot_img_hdr *Config::GetHeader()
     memset(&it_cfg, 0x00, sizeof (boot_img_hdr));
 
     if (options[PROP_KERNEL_ADDR].len > 0)
-        sscanf(options[PROP_KERNEL_ADDR].txtname, "0x%08x", &it_cfg.kernel_addr);
+	sscanf(options[PROP_KERNEL_ADDR].txtname, "0x%08x", &it_cfg.kernel_addr);
 
     if (options[PROP_KERNELTAG_ADDR].len > 0)
-        sscanf(options[PROP_KERNELTAG_ADDR].txtname, "0x%08x", &it_cfg.tags_addr);
+	sscanf(options[PROP_KERNELTAG_ADDR].txtname, "0x%08x", &it_cfg.tags_addr);
 
     if (options[PROP_ROOTFS_ADDR].len > 0)
-        sscanf(options[PROP_ROOTFS_ADDR].txtname, "0x%08x", &it_cfg.ramdisk_addr);
+	sscanf(options[PROP_ROOTFS_ADDR].txtname, "0x%08x", &it_cfg.ramdisk_addr);
 
     if (options[PROP_SECONDFS_ADDR].len > 0)
-        sscanf(options[PROP_SECONDFS_ADDR].txtname, "0x%08x", &it_cfg.second_addr);
+	sscanf(options[PROP_SECONDFS_ADDR].txtname, "0x%08x", &it_cfg.second_addr);
 
     if (options[PROP_PAGE_SIZE].len > 0)
-        sscanf(options[PROP_PAGE_SIZE].txtname, "%d", &it_cfg.page_size);
+	sscanf(options[PROP_PAGE_SIZE].txtname, "%d", &it_cfg.page_size);
     else
-        it_cfg.page_size = 0x800;
+	it_cfg.page_size = 0x800;
 
     if (options[MIX_OS_VERSION].len > 0) {
-        sscanf(options[MIX_OS_VERSION].txtname, "%d.%d.%d %02d/%d", &ver.major, &ver.minor, &ver.patch, &ver.month, &ver.year);
-        it_cfg.os_version = PackOSVersion(&ver);
+	sscanf(options[MIX_OS_VERSION].txtname, "%d.%d.%d %02d/%d", &ver.major, &ver.minor, &ver.patch, &ver.month, &ver.year);
+	it_cfg.os_version = PackOSVersion(&ver);
     }
 
     if (options[PROP_NAME].len > 0)
-        sscanf(options[PROP_NAME].txtname, "%s", (char *) &it_cfg.name);
+	strncpy((char *) it_cfg.name, options[PROP_NAME].txtname, BOOT_NAME_SIZE);
 
     if (options[PROP_CMDLINE].len > 0)
-        sscanf(options[PROP_CMDLINE].txtname, "%s", (char *) &it_cfg.cmdline);
+	strncpy((char *) it_cfg.cmdline, options[PROP_NAME].txtname, BOOT_ARGS_SIZE);
 
     if (options[PROP_CMDLINE_EX].len > 0)
-        sscanf(options[PROP_CMDLINE_EX].txtname, "%s", (char *) &it_cfg.extra_cmdline);
+	strncpy((char *) it_cfg.extra_cmdline, options[PROP_NAME].txtname, BOOT_EXTRA_ARGS_SIZE);
 
     if (options[ID0].len > 0)
-        for (int i = 0; i < 8; i++) {
-            sscanf(&options[ID0].txtname[i * 8], "%08x", &it_cfg.id[i]);
-        }
+	for (int i = 0; i < 8; i++) {
+	    sscanf(&options[ID0].txtname[i * 8], "%08x", &it_cfg.id[i]);
+	}
 
 
 
@@ -308,19 +308,19 @@ boot_img_hdr *Config::GetHeader()
 }
 
 int Config::WriteCfg(UfNtype *file)
-{   //Выплевывает текстовый конфиг из загруженного ранее
+{ //Выплевывает текстовый конфиг из загруженного ранее
     FILE *fh = NULL;
     char str[2048];
     int counter = 0;
     if ((fh = fopen(file, "wt")) == NULL)
-        return counter;
+	return counter;
 
     for (int i = 0; i < NUM_PROPS; i++) {
-        if (options[i].len > 0) {
-            sprintf(str, "%s=%s\r\n", names[i], options[i].txtname);
-            fwrite(str, 1, strlen(str), fh);
-            counter++;
-        }
+	if (options[i].len > 0) {
+	    sprintf(str, "%s=%s\r\n", names[i], options[i].txtname);
+	    fwrite(str, 1, strlen(str), fh);
+	    counter++;
+	}
     }
     fclose(fh);
     return counter;
